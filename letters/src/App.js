1994f78f4a2bc3535d2a480cc0ed8513d303721d
Marcode777 <marcosebello7@gmail.com>
2017-05-11 21:38:40 -0400

    starting to set up html structure

--- letters/src/App.js
+++ letters/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import EasyABC from './easyabc';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h3>letters</h3>
+        <EasyABC/>
       </div>
     );
   }
