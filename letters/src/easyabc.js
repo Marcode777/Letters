2f4f6954f808c070f99c645e80b6ba63f21ff8cf
Marcode777 <marcosebello7@gmail.com>
2017-05-14 07:19:54 -0400

    added additionl fields, field-block, left-field and right-field divs

--- letters/src/easyabc.js
+++ letters/src/easyabc.js
@@ -16,6 +16,12 @@ export default class EasyABC extends React.Component{
               <a href="#" className="button sound"> Play Sound</a>
               <a href="#" className="button next">Next</a>
             </div>
+            <div className="fields">
+              <div className="field-block">
+                <div className="left-field">A</div>
+                <div className="right-field">A</div>
+              </div>
+            </div>
           </div>
         </div>
       );
