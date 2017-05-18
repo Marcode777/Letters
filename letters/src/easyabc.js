88c51761799b99833b92efad505907d00f60da0f
Marcode777 <marcosebello7@gmail.com>
2017-05-18 03:23:49 -0400

    added dynamic image alts, image div panel and word div panel now display corresponding dynamic json content

--- letters/src/easyabc.js
+++ letters/src/easyabc.js
@@ -32,11 +32,14 @@ export default class EasyABC extends React.Component{
                   <div className="placeholder-span">
                     Click Next to View Image
                   </div>
-                    <img className="letter-image" src={this.state.alphabets[this.state.currentPosition].image}/>
+                    <img className="letter-image" alt={this.state.alphabets[this.state.currentPosition].word} src={this.state.alphabets[this.state.currentPosition].image}/>
                 </div>
                 <div className="right-field">
                   <div className="placeholder-span">
                     Click Next to View the Spelling
+                  </div>
+                  <div className="word">
+                    {this.state.alphabets[this.state.currentPosition].word}
                   </div>
                 </div>
               </div>
